c527c5b88ca85d870a3117972c574e56be814008
Mohamed-Zkaria <mohamed.zakaria_10_7@yahoo.com>
2021-03-08 12:57:03 +0200

    connecting to database and adding a .env example

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
-const PORT = 5000;
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://${process.env.SERVER}/garment`, {useNewUrlParser: true, useUnifiedTopology: true}, (error)=>{
+    if(error){
+        console.log(error)
 
-app.listen(PORT, (error)=>{
+    } else {
+        console.log("connected to database");
+    }
+});
+
+
+app.listen(process.env.SERVERPORT, (error)=>{
     if(error){
         console.log(error);
     } else {
-        console.log(`Server is listening on port: ${PORT}`);
+        console.log(`Server is listening on port: ${process.env.SERVERPORT}`);
     }
 })
